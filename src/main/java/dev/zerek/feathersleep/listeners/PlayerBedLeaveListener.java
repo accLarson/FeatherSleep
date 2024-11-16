c24b92d70f1a39f58ae6b4e156ed42f0474402c0
accLarson <acc.larson@gmail.com>
2024-11-15 19:17:47 -0500

    Wake up message on any wakeup

--- src/main/java/dev/zerek/feathersleep/listeners/PlayerBedLeaveListener.java
+++ src/main/java/dev/zerek/feathersleep/listeners/PlayerBedLeaveListener.java
@@ -18,10 +18,6 @@ public class PlayerBedLeaveListener implements Listener {
     public void onPlayerBedLeave(PlayerBedLeaveEvent event) {
         plugin.getSleepManager().storeSleepingPlayerStatus(event.getPlayer(),false);
         plugin.getServer().getScheduler().runTaskLater(plugin, () -> plugin.getSleepManager().recalculate(), 1L);
-
-        long time = event.getPlayer().getWorld().getTime();
-        if (time == 0 || (time >= 23500 && time <= 23600)) {
-            event.getPlayer().sendActionBar(MiniMessage.miniMessage().deserialize(this.plugin.getConfigManager().getWakeUpMessage()));
-        }
+        event.getPlayer().sendActionBar(MiniMessage.miniMessage().deserialize(this.plugin.getConfigManager().getWakeUpMessage()));
     }
 }
